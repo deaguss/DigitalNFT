471154a1f01162304062adcec3da6bad3bf036e2
deagus <dedeaskyt121@gmail.com>
2024-05-29 18:27:25 +0800

    update

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -16,80 +16,80 @@ const addUser: BeforeChangeHook<Product> = async ({
   return { ...data, user: user.id }
 }
 
-// const syncUser: AfterChangeHook<Product> = async ({
-//   req,
-//   doc,
-// }) => {
-//   const fullUser = await req.payload.findByID({
-//     collection: 'users',
-//     id: req.user.id,
-//   })
+const syncUser: AfterChangeHook<Product> = async ({
+  req,
+  doc,
+}) => {
+  const fullUser = await req.payload.findByID({
+    collection: 'users',
+    id: req.user.id,
+  })
 
-//   if (fullUser && typeof fullUser === 'object') {
-//     const { products } = fullUser
+  if (fullUser && typeof fullUser === 'object') {
+    const { products } = fullUser
 
-//     const allIDs = [
-//       ...(products?.map((product) =>
-//         typeof product === 'object' ? product.id : product
-//       ) || []),
-//     ]
+    const allIDs = [
+      ...(products?.map((product) =>
+        typeof product === 'object' ? product.id : product
+      ) || []),
+    ]
 
-//     const createdProductIDs = allIDs.filter(
-//       (id, index) => allIDs.indexOf(id) === index
-//     )
+    const createdProductIDs = allIDs.filter(
+      (id, index) => allIDs.indexOf(id) === index
+    )
 
-//     const dataToUpdate = [...createdProductIDs, doc.id]
+    const dataToUpdate = [...createdProductIDs, doc.id]
 
-//     await req.payload.update({
-//       collection: 'users',
-//       id: fullUser.id,
-//       data: {
-//         products: dataToUpdate,
-//       },
-//     })
-//   }
-// }
+    await req.payload.update({
+      collection: 'users',
+      id: fullUser.id,
+      data: {
+        products: dataToUpdate,
+      },
+    })
+  }
+}
 
-// const isAdminOrHasAccess =
-//   (): Access =>
-//   ({ req: { user: _user } }) => {
-//     const user = _user as User | undefined
+const isAdminOrHasAccess =
+  (): Access =>
+  ({ req: { user: _user } }) => {
+    const user = _user as User | undefined
 
-//     if (!user) return false
-//     if (user.role === 'admin') return true
+    if (!user) return false
+    if (user.role === 'admin') return true
 
-//     const userProductIDs = (user.products || []).reduce<
-//       Array<string>
-//     >((acc, product) => {
-//       if (!product) return acc
-//       if (typeof product === 'string') {
-//         acc.push(product)
-//       } else {
-//         acc.push(product.id)
-//       }
+    const userProductIDs = (user.products || []).reduce<
+      Array<string>
+    >((acc, product) => {
+      if (!product) return acc
+      if (typeof product === 'string') {
+        acc.push(product)
+      } else {
+        acc.push(product.id)
+      }
 
-//       return acc
-//     }, [])
+      return acc
+    }, [])
 
-//     return {
-//       id: {
-//         in: userProductIDs,
-//       },
-//     }
-//   }
+    return {
+      id: {
+        in: userProductIDs,
+      },
+    }
+  }
 
 export const Products: CollectionConfig = {
   slug: 'products',
   admin: {
     useAsTitle: 'name',
   },
-  // access: {
-  //   read: isAdminOrHasAccess(),
-  //   update: isAdminOrHasAccess(),
-  //   delete: isAdminOrHasAccess(),
-  // },
+  access: {
+    read: isAdminOrHasAccess(),
+    update: isAdminOrHasAccess(),
+    delete: isAdminOrHasAccess(),
+  },
   hooks: {
-    // afterChange: [syncUser],
+    afterChange: [syncUser],
     beforeChange: [
       addUser,
       async (args) => {
